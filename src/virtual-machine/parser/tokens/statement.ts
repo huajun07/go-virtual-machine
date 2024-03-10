200ef776da6c51c78969c7debbe21d4df00fe485
An Jun <limanjun99@gmail.com>
2024-03-10 15:41:29 +0800

    feat: tokenize assignment statements

--- src/virtual-machine/parser/tokens/statement.ts
+++ src/virtual-machine/parser/tokens/statement.ts
@@ -1,4 +1,23 @@
+import { Token } from './base'
 import { ExpressionToken } from './expressions'
 
 //! TODO (P1): Add other types of statements and expressions
 export type StatementToken = ExpressionToken
+
+export class AssignmentStatementToken extends Token {
+  left: ExpressionToken[]
+  /** One of "=", "+=" and "*=" */
+  operation: string
+  right: ExpressionToken[]
+
+  constructor(
+    left: ExpressionToken[],
+    operation: string,
+    right: ExpressionToken[],
+  ) {
+    super('assignment')
+    this.left = left
+    this.operation = operation
+    this.right = right
+  }
+}
