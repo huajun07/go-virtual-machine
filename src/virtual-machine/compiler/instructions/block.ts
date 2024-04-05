2b449e6f61f1871ad3f6f459d26ae56ec23dfe14
An Jun <limanjun99@gmail.com>
2024-04-05 14:52:36 +0800

    refactor: deduplicate code

--- src/virtual-machine/compiler/instructions/block.ts
+++ src/virtual-machine/compiler/instructions/block.ts
@@ -48,10 +48,27 @@ export class BlockInstruction extends Instruction {
         new_frame.set_idx(FuncNode.default(process.heap).addr, i)
       } else throw Error('Unsupported Type')
     }
-    process.context.pushRTS(
-      process.context.E().extend_env(new_frame.addr, this.for_block).addr,
-    )
+
+    if (!(this instanceof FuncBlockInstruction)) {
+      process.context.pushRTS(
+        process.context.E().extend_env(new_frame.addr, this.for_block).addr,
+      )
+    } else {
+      // This is to not trigger the exit scope condition of the closure env
+      process.context.set_E(
+        process.context.E().extend_env(new_frame.addr, this.for_block).addr,
+      )
+    }
     process.heap.temp_roots.pop()
+    if (this instanceof FuncBlockInstruction) {
+      for (let i = this.args - 1; i >= 0; i--) {
+        const src = process.context.popOS()
+        const dst = new_frame.get_idx(i)
+        process.heap.copy(dst, src)
+      }
+      // Pop function in stack
+      process.context.popOS()
+    }
   }
 }
 export class FuncBlockInstruction extends BlockInstruction {
@@ -60,38 +77,6 @@ export class FuncBlockInstruction extends BlockInstruction {
     super(false)
     this.tag = 'FUNC_BLOCK'
     this.args = args
-  }
-
-  override execute(process: Process): void {
-    const new_frame = FrameNode.create(this.frame.length, process.heap)
-    process.heap.temp_roots.push(new_frame.addr)
-    for (let i = 0; i < this.frame.length; i++) {
-      const T = this.frame[i]
-      if (T instanceof BoolType) {
-        new_frame.set_idx(BoolNode.default(process.heap).addr, i)
-      } else if (T instanceof Int64Type) {
-        new_frame.set_idx(IntegerNode.default(process.heap).addr, i)
-      } else if (T instanceof Float64Type) {
-        new_frame.set_idx(FloatNode.default(process.heap).addr, i)
-      } else if (T instanceof StringType) {
-        new_frame.set_idx(StringNode.default(process.heap).addr, i)
-      } else if (T instanceof FunctionType) {
-        new_frame.set_idx(FuncNode.default(process.heap).addr, i)
-      } else throw Error('Unsupported Type')
-    }
-
-    // This is to not trigger the exit scope condition of the closure env
-    process.context.set_E(
-      process.context.E().extend_env(new_frame.addr, this.for_block).addr,
-    )
-    process.heap.temp_roots.pop()
-    for (let i = this.args - 1; i >= 0; i--) {
-      const src = process.context.popOS()
-      const dst = new_frame.get_idx(i)
-      process.heap.copy(dst, src)
-    }
-    // Pop function in stack
-    process.context.popOS()
   }
 }
 
