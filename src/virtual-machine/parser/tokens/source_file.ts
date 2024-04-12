630e7bea39467e43ffd9a2e15726d02c39d92728
An Jun <limanjun99@gmail.com>
2024-04-12 23:58:12 +0800

    feat: add true and false keywords

--- src/virtual-machine/parser/tokens/source_file.ts
+++ src/virtual-machine/parser/tokens/source_file.ts
@@ -2,9 +2,11 @@ import { Compiler } from '../../compiler'
 import {
   BlockInstruction,
   CallInstruction,
+  LoadConstantInstruction,
   LoadVariableInstruction,
+  StoreInstruction,
 } from '../../compiler/instructions'
-import { NoType, Type } from '../../compiler/typing'
+import { BoolType, NoType, Type } from '../../compiler/typing'
 import { builtinPackages } from '../../compiler/typing/packages'
 
 import { Token } from './base'
@@ -21,14 +23,24 @@ export class SourceFileToken extends Token {
   }
 
   override compile(compiler: Compiler): Type {
+    // Setup.
     const global_block = new BlockInstruction('GLOBAL')
     compiler.instructions.push(global_block)
     compiler.context.push_env()
     compiler.type_environment = compiler.type_environment.extend()
+
+    // Compile imports.
     for (const imp of this.imports ?? []) imp.compile(compiler)
+
+    // Declare builtin constants for `true` and `false`.
+    this.predeclareConstants(compiler)
+
+    // Compile top level declarations.
     for (const declaration of this.declarations || []) {
       declaration.compile(compiler)
     }
+
+    // Call main function.
     const [frame_idx, var_idx] = compiler.context.env.find_var('main')
     compiler.instructions.push(
       new LoadVariableInstruction(frame_idx, var_idx, 'main'),
@@ -40,6 +52,31 @@ export class SourceFileToken extends Token {
     )
     global_block.set_identifiers(vars)
     return new NoType()
+  }
+
+  private predeclareConstants(compiler: Compiler): void {
+    const constants = [
+      {
+        name: 'true',
+        loadInstruction: new LoadConstantInstruction(true, new BoolType()),
+        type: new BoolType(),
+      },
+      {
+        name: 'false',
+        loadInstruction: new LoadConstantInstruction(false, new BoolType()),
+        type: new BoolType(),
+      },
+    ]
+    for (const constant of constants) {
+      const { name, loadInstruction, type } = constant
+      const [frame_idx, var_idx] = compiler.context.env.declare_var(name)
+      compiler.instructions.push(
+        loadInstruction,
+        new LoadVariableInstruction(frame_idx, var_idx, name),
+        new StoreInstruction(),
+      )
+      compiler.type_environment.addType(name, type)
+    }
   }
 }
 
